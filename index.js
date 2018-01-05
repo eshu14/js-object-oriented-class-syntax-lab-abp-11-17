4ddce7324e50d74edc00f9f5aa9e8efbeb388ba7
Easwari Ramani <eshuprad@gmail.com>
2018-01-05 15:24:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ class BoardMember
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${training}.`
   }
 }
+class Ceo extends BoardMember
+{
+  hireEmployee(){
+    return "Welcome aboard!";
+  }
+}
